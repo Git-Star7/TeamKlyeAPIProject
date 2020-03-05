f79adabeb287c189c4646d6943b542b988bc4c5b
Michael <Michael@acdmail.com>
2020-03-05 11:48:13 -0500

    first commit

---
+++ GroupMovieAPIProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GroupMovieAPIProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
